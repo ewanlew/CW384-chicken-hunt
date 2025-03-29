db7435b3736e458f9b6693ee48a0b9efa8dbe32f
ewanlew <97612083+ewanlew@users.noreply.github.com>
2025-03-29 23:09:26 +0000

    Back button auto hides

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
+    public GameObject SettingsMenu;
+
+    void Start() {
+        SettingsMenu.SetActive(false);
+    }
+
+    public void PlayGame() {
         SceneManager.LoadScene(1);
     }
 
-    public void QuitGame(){
+    public void QuitGame() {
         Application.Quit();
+        Debug.Log("Game is exiting");
     }
 }
