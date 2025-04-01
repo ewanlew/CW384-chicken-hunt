fe1c69aa54a8ce03b2bf71793411df6529e99e15
ewanlew <97612083+ewanlew@users.noreply.github.com>
2025-04-01 20:51:28 +0100

    adjust grav scale & spawning

--- Assets/Scripts/ChickenSpawner.cs
+++ Assets/Scripts/ChickenSpawner.cs
@@ -18,14 +18,25 @@ public class ChickenSpawner : MonoBehaviour
 
     void SpawnChicken()
     {
-        float x = Camera.main.transform.position.x + Random.Range(-scrollRange, scrollRange);
-        Vector3 spawnPos = new Vector3(x, -6f, 0f);
+        float screenBottomY = -6f;
+
+        // range of spawn
+        float camHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        float spawnX = Camera.main.transform.position.x + Random.Range(-camHalfWidth - scrollRange, camHalfWidth + scrollRange);
+        Vector3 spawnPos = new Vector3(spawnX, -6f, 0f);
+
         GameObject chicken = Instantiate(chickenPrefab, spawnPos, Quaternion.identity);
 
-        float upwardSpeed = Random.Range(1.5f, 3f);
-        chicken.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, upwardSpeed);
+        // vector to centre w/ offset
+        float toCenter = Camera.main.transform.position.x - spawnX;
+        float xVelocity = (toCenter * 0.3f) + Random.Range(-1f, 1f);
+        float yVelocity = Random.Range(5f, 8f);
 
-        Destroy(chicken, 10f);
+        // init velocity upward -> centre
+        chicken.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(xVelocity, yVelocity);
+
+        // KILL chicken after a lil
+        Destroy(chicken, 12f);
     }
 
 }
