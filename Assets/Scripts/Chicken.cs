2e64acac98befd3b0a8743f13202cd3e4b6d2bfe
ewanlew <97612083+ewanlew@users.noreply.github.com>
2025-04-03 01:02:51 +0100

    chicken weeee animation

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
+using System.Collections;
 
 public class Chicken : MonoBehaviour
 {
-
     public float killRadius = 0.5f;
 
     public void TryHit(Vector2 clickPosition) { 
@@ -10,13 +10,44 @@ public class Chicken : MonoBehaviour
 
         if (distance <= killRadius) {
             GameManager.Instance.AddScore(1);
+            StartCoroutine(HitAnimation());
             Debug.Log("hit!");
         } else {
             GameManager.Instance.Miss();
             Debug.Log("close but missed...");
         }
+    }
+
+    IEnumerator HitAnimation() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        // stops velocity of chicken
+        if (rb) {
+            rb.linearVelocity = Vector2.zero; 
+        }
+
+        float duration = 0.2f;
+        float time = 0f;
+
+        Color startColor = sr.color;
+        Vector3 startScale = transform.localScale;
+
+        while (time < duration) {
+            float t = time / duration;
+
+            // shrinks chicken
+            transform.localScale = Vector3.Lerp(startScale, startScale * 0.2f, t);
+
+            // fade to white-ish
+            sr.color = Color.Lerp(startColor, new Color(1f, 1f, 1f, 0.2f), t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
 
         Destroy(gameObject);
     }
+
 
 }
